a973471b50e198e144a1beea657f60a90732c1a1
mayurwagh <mayurwagh1412@gmail.com>
2025-04-19 14:46:55 +0530

    frontend code is added

---
+++ pipeline.groovy
@@ -0,0 +1,25 @@
+pipeline{
+    agent any 
+    stages {
+        stage('pull'){
+            steps{
+                git branch: 'main', url: 'git@github.com:mayurmwagh/Newfrontend.git'
+            }
+        }
+        stage('build'){
+            steps{
+                sh '''
+                    npm install
+                    ng build 
+                '''
+            }
+        }
+        stage('deploy'){
+            steps{
+                sh '''
+                    aws s3 cp --recursive dist/angular-frontend s3://cbz-frontend-project-bux123-king/
+                '''
+            }
+        }
+    }
+}
